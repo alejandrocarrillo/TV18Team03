95b382ce0d768e0030f7e5b74bae77ad17021837
Mike Sagnelli <ing.sagnelli@gmail.com>
2018-03-05 17:29:42 -0600

    Finished maze prototype

---
+++ tv18team3/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody rb;
+	private bool walking;
+	float speed,
+		  timeCount,
+		  animSeconds;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		this.walking = false;
+		this.speed = 50.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Conditional to detect if character can walk or not
+		if (this.walking) {
+			this.rb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+			this.rb.velocity = Camera.main.transform.forward * speed * Time.deltaTime;
+		}
+	}
+
+	// Setter for walking capability
+	public void setWalkingTrue(){
+		this.walking = true;
+	}
+
+	public void setWalkingFalse(){
+		this.walking = false;
+	}
+
+
+}
