78466c82bf1f472dbfee92e1a2a9f3506a785e42
Mike Sagnelli <ing.sagnelli@gmail.com>
2018-03-06 17:14:27 -0600

    Solved issue 4 & 6

---
+++ tv18team3/Assets/Scripts/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour{
+
+	public string name;
+	public string type;
+	public float benefit;
+
+	void Start(){
+
+	}
+
+	void Update(){
+
+	}
+
+}
