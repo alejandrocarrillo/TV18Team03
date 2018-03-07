3cae9d6c32477e40e3e1d3569dd74b77bcef3c88
Alex2197 <35312909+Alex2197@users.noreply.github.com>
2018-03-07 13:39:12 -0600

    Web initialization

---
+++ Web/TallerVertical18/TallerVertical18/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TallerVertical18.Startup))]
+namespace TallerVertical18
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
