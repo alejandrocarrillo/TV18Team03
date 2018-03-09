e3d5cf4cd375e5c1182f53ea2034f7f1efa843d5
Mike Sagnelli <ing.sagnelli@gmail.com>
2018-03-08 20:19:55 -0600

    Added Enemies with AIs, and Wanderers with AIs. Nav Mesh as well.

---
+++ tv18team3/Assets/Previous/Scripts/EnemyAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class EnemyAI : MonoBehaviour {
+
+	private Transform target;
+	private Vector3 dest;
+	private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find ("Player").transform;
+		agent = GetComponent<NavMeshAgent> ();
+		dest = agent.destination;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (dest, target.position) < 10.0f) {
+			dest = target.position;
+			agent.destination = dest;
+		}
+	}
+}
---
+++ tv18team3/Assets/Previous/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Rigidbody rb;
+	private bool walking;
+	private float speed;
+	private AudioSource footsteps;
+
+	// Use this for initialization
+	void Start () {
+		this.rb = GetComponent<Rigidbody> ();
+
+		this.footsteps = GetComponent<AudioSource> ();
+
+		this.walking = false;
+
+		this.speed = 150.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Conditional to detect if character can walk or not
+		if (this.walking) {
+			this.rb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+			this.rb.velocity = Camera.main.transform.forward * speed * Time.deltaTime;
+		} else {
+			this.rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+		}
+	}
+
+	// Setter for walking capability
+	public void isWalking(){
+		this.walking = true;
+		this.footsteps.Play ();
+	}
+
+	public void isNotWalking(){
+		this.walking = false;
+		this.footsteps.Pause ();
+	}
+}
